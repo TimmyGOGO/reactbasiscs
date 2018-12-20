4c817cd494843115ab9e047f80ba96c65ad3d82e
TimmyGOGO <temich941@mail.ru>
2018-12-20 12:05:12 +0300

    Updates

---
+++ precache-manifest.418f58dbc937fa0ee0208d499d508bb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactbasiscs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "19ae55bf863143d000d6",
+    "url": "/reactbasiscs/static/js/runtime~main.19ae55bf.js"
+  },
+  {
+    "revision": "7ab2674ce58f4b4ac863",
+    "url": "/reactbasiscs/static/js/main.7ab2674c.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/reactbasiscs/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7ab2674ce58f4b4ac863",
+    "url": "/reactbasiscs/static/css/main.a544d74f.chunk.css"
+  },
+  {
+    "revision": "3eff59f704a04131d29be566866b1f7c",
+    "url": "/reactbasiscs/index.html"
+  }
+];
